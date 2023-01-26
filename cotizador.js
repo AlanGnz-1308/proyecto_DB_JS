ffe61dbb14446cdf206dd904a6c6f7c9ac3e83db
Alan Gonzalez <gonzalez.alan95@hotmail.com>
2023-01-26 20:57:16 -0300

    Agrega condicional para menor a 50000

--- cotizador.js
+++ cotizador.js
@@ -1,41 +1,40 @@
 const IUS = 5787
 
-let servElegido = prompt("Elija los servicios que desea cotizar según el cuadro escribiendo en Mayúsculas: CONSULTA VERBAL, CONSULTA ESCRITA, CONTRATOS, ASISTENCIA A MEDIACION y luego escriba FINALIZAR: ");
+let servElegido = prompt("Elija y escriba EN MAYÚSCULAS los servicios que desea cotizar según el cuadro escribiendo: CONSULTA VERBAL, CONSULTA ESCRITA, CONTRATOS, ASISTENCIA A MEDIACION y luego escriba FINALIZAR: ");
 
-function verbal() {
-    return IUS*3
-}
-
-function escrita() {
-    return IUS*1
-}
-
-function contrato() {
-    return IUS*20
-}
-
-function mediacion(){
-    return IUS*5
+function calculo_IUS() {
+    if(servElegido=="CONSULTA VERBAL"){
+        return IUS*3
+    }
+    else if(servElegido=="CONSULTA ESCRITA"){
+        return IUS*1
+    }
+    else if(servElegido=="CONTRATOS"){
+        return IUS*20
+    }
+    else if(servElegido=="ASISTENCIA A MEDIACION"){
+        return IUS*5
+    }
 }
 
 let sumaRealizada = 0
 
 while (servElegido.toUpperCase() != "FINALIZAR") {
     if(servElegido=="CONSULTA VERBAL"){
-        sumaRealizada = verbal()+sumaRealizada
+        sumaRealizada = calculo_IUS()+sumaRealizada
         servElegido = prompt("Ingrese otro servicio (puede guiarse con el cuadro) o FINALIZAR para visualizar la cotización")
     }
     else if(servElegido=="CONSULTA ESCRITA"){
-        sumaRealizada = escrita()+sumaRealizada
+        sumaRealizada = calculo_IUS()+sumaRealizada
         servElegido = prompt("Ingrese otro servicio (puede guiarse con el cuadro) o FINALIZAR para visualizar la cotización")
     }
     else if(servElegido=="CONTRATOS"){
-        sumaRealizada = contrato()+sumaRealizada
+        sumaRealizada = calculo_IUS()+sumaRealizada
         servElegido = prompt("Ingrese otro servicio (puede guiarse con el cuadro) o FINALIZAR para visualizar la cotización")
     }
     else if(servElegido=="ASISTENCIA A MEDIACION"){
-            sumaRealizada = mediacion()+sumaRealizada
-            servElegido = prompt("Ingrese otro servicio (puede guiarse con el cuadro) o FINALIZAR para visualizar la cotización")
+        sumaRealizada = calculo_IUS()+sumaRealizada
+        servElegido = prompt("Ingrese otro servicio (puede guiarse con el cuadro) o FINALIZAR para visualizar la cotización")
     }
     else{
         servElegido = prompt ("recuerde que puede escribir en mayúsculas solamente CONSULTA VERBAL, CONSULTA ESCRITA, CONTRATO, ASISTENCIA A MEDIACION o FINALIZAR")
@@ -48,4 +47,6 @@ alert("La cotización total es de: $"+sumaRealizada)
 if (sumaRealizada > 50000) {
     alert("Como su cotización es mayor a $50.000 usted puede pagar con tarjeta de crédito en 3 cuotas sín interes de: $"+(sumaRealizada/3))
 }
-
+else if(sumaRealizada < 50000){
+    alert("Como su cotización es menor a $50.000 usted puede pagar en efectivo o con tarjeta de crédito en 3 cuotas CON interés 10% de: $"+((sumaRealizada*0,10)+sumaRealizada/3))
+}
